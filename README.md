6389143117f46612a812c1b9b0f566b0a48846c6
ussaohelcim <57050328+ussaohelcim@users.noreply.github.com>
2021-06-22 17:27:48 -0300

    versão inicial

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # nodechan
-Imageboard utilizando nodejs como backend.
+Imageboard simples utilizando nodejs como backend.
+
+# Estado do desenvolvimento
+
+- [X] Receber OPs na pagina inicial
+- [X] Clicar em uma para ir até essa postagem
+- [X] Receber respostas ao entrar em uma postagem
+- [ ] Escrever assunto
+- [ ] Escrever mensagem
+- [ ] Enviar imagem
+- [ ] Criar nova postagem
+    - [ ] Via cliente
+    - [X] Via servidor
+- [ ] Criar nova resposta
+    - [ ] Via cliente
+    - [X] Via servidor
+
+
+# ESTOU FAZENDO APENAS PELO ESTUDO, ESSE SERVIDOR NÃO É SEGURO
---
+++ main.js
@@ -0,0 +1,133 @@
+const http = require('http')
+
+const hostname = "192.168.0.105"
+const port = 80
+const fs = require('fs')
+const db = require("./threads.json")
+
+let teste = `
+<div class="op">
+    <div class="imagem"></div>
+    <div class="conteudo">
+        <p>Anonimo</p>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus nam quo sequi, aspernatur nostrum vitae id amet illum quas quos tempore, facere quasi porro quia omnis minus cupiditate consectetur accusantium.</p>
+    </div> 
+</div>
+`
+let fioResposta = `
+<div class="novo">
+    <div class="imagem"></div>
+    <div class="conteudo">
+        <h2>Responder</h2>
+        <input placeholder="Assunto" type="text">
+        <textarea placeholder="Texto da sua resposta" name="" id="" cols="30" rows="10"></textarea>
+        <button type="submit">Enviar</button>
+    </div> 
+</div>` 
+const restoSite = `</div></main></body></html>`
+
+const servidor = http.createServer((request,response)=>{
+    response.statusCode = 200;
+    response.setHeader('Content-Type', 'text/html');
+    let url = request.url;
+    
+    //console.log(request.param)
+    
+    if(request.method == 'POST')
+    {
+        console.log("tentou um post")
+    }
+    if(url === '/')
+    {
+        //home
+        fs.readFile("catalogo.html", function(err, site){
+            if(err)
+            {
+                response.writeHead(404, {'Content-Type': 'text/html'});
+                return response.end("404 Not Found");
+            }
+
+            response.write(site+getOPs()+restoSite)
+            return response.end();
+        })
+    }
+    else if(url.startsWith("/fio/"))
+    {
+        let fio = Number(url.replace("/fio/","")) 
+        fs.readFile("index.html", function(err, site){
+            if(err)
+            {
+                response.writeHead(404, {'Content-Type': 'text/html'});
+                return response.end("404 Not Found");
+            }
+
+            response.write(site+getFio(fio)+fioResposta+restoSite)
+            return response.end();
+        })
+    }
+    else
+    {
+        //404
+        response.statusCode = 404;
+        fs.readFile("404.html", function(err, site){
+            if(err)
+            {
+                response.writeHead(404, {'Content-Type': 'text/html'});
+                return response.end("404 Not Found");
+            }
+
+            response.write(site)
+            return response.end();
+        })
+    }
+})
+servidor.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+    console.log(db.fios[0].OP)
+    //novoFio("Salve","","Estou apenas tostando o pretiffy")
+});
+
+function getFio(numero)
+{
+    let fiojson = db.fios[numero-1]
+    let op = `<div class="fio"><div class="op"><div class="imagem"></div><div class="conteudo"><p>${fiojson.OP.titulo}</p><p>${fiojson.OP.mensagem}</p></div></div>`
+    let respostas = ""
+    for (let index = 0; index < fiojson.respostas.length; index++) {
+        respostas+=`<div class="resposta"><div class="imagem"></div><div class="conteudo">
+            <p>${fiojson.respostas[index].titulo == ""? "Anonimo" : fiojson.respostas[index].titulo}</p>
+            <p>${fiojson.respostas[index].mensagem}</p>
+        </div>
+    </div>`
+    }
+
+    return op+respostas
+}
+function getOPs()
+{
+    let fiosJson = db.fios
+    let ops = ""
+    for (let i = 0; i < fiosJson.length; i++) 
+    {
+        ops += `<div class="fio" onclick="window.location='${"/fio/"+fiosJson[i].OP.numero}'">
+            <div class="imagem"></div>
+            <div class="conteudo"><p>${fiosJson[i].OP.titulo}</p><p>${fiosJson[i].OP.mensagem}</p></div>
+            </div>`
+    }
+    return ops;
+}
+function novoFio(titulo,imagem,comentario)
+{
+    db.fios.push({OP:{numero:db.fios.length+1,titulo:titulo,imagem:"",mensagem:comentario},respostas:[]})
+    fs.writeFile("threads.json",JSON.stringify(db),err =>{
+        if(err) throw err
+        console.log("Novo fio adicionado")
+    })
+}
+function novaResposta(fio,title,imagem,comment)
+{
+    db.fios[fio-1].respostas.push({titulo:title,imagem:"",comentario:comment})
+    fs.writeFile("threads.json",JSON.stringify(db),err =>{
+        if(err) throw err
+        console.log("Nova resposta adicionada")
+    })
+}
